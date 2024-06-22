9d14fda12815ecd5db68f78313501fdc124080f4
arcsagar <arcsagar15@gmail.com>
2024-06-22 10:14:18 +0530

    task 1 completed

---
+++ ecom_select_where_query.sql
@@ -0,0 +1,40 @@
+select * from sales
+
+select order_line from sales
+
+select * from sales
+
+select order_line, customer_id, product_id,sales from sales
+
+
+select * from sales
+
+select distinct ship_mode from sales
+
+select product_id from sales
+
+
+select distinct product_id from sales
+
+
+select distinct ship_mode , product_id from sales
+
+select * from sales where sales > 500
+
+select * from sales where ship_mode = 'Same Day'
+
+select * from sales where product_id = 'OFF-SU-10001225' OR product_id = 'OFF-AR-10002833'
+
+select * from sales where sales > 500 AND sales < 800
+
+select * from sales where sales > 500 OR quantity > 5\
+
+
+-- creat
+
+delete from customer
+
+select * from customer
+
+select * from customer
+
